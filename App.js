87188156d472c04dd32d2bacb6f27defb704da63
darren11358555 <152318064+darren11358555@users.noreply.github.com>
2024-05-27 09:02:36 -0700

    Update App.js
    here I made a text id so i can edit the font of my text

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
+
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My Name is Darren</Text>
+      <Text style={styles.text}>
+        My name is Darren</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: 'lightgreen',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  text: {
+    fontSize: 24,
+    color: "white",
